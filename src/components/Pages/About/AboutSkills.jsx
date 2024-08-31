3a062d94b9db0f2a502bcd14fd2266d4d7a757b4
tanmayhere <tanmayhere23@gmail.com>
2024-08-31 17:27:44 +0530

    abc

--- src/components/Pages/About/AboutSkills.jsx
+++ src/components/Pages/About/AboutSkills.jsx
@@ -32,10 +32,10 @@ const badgeData = [
       src: 'https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white',
       alt: 'tailwind',
    },
-   {
-      src: 'https://img.shields.io/badge/Vite-B73BFE?style=for-the-badge&logo=vite&logoColor=FFD62E',
-      alt: 'vite',
-   },
+  // {
+  //    src: 'https://img.shields.io/badge/Vite-B73BFE?style=for-the-badge&logo=vite&logoColor=FFD62E',
+  //    alt: 'vite',
+  // },
    {
       src: 'https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB',
       alt: 'react',
